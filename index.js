e3eac875610771d074d66464bc2d2005fdab2123
zaru@sakuraba <zarutofu@gmail.com>
2016-04-05 00:32:46 +0900

    change arrow function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var socketio = require('socket.io');
 var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
@@ -14,26 +14,26 @@ var io = socketio.listen(server);
 var rooms = [];
 var messages = {};
 
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
 
     io.emit('fetch rooms', rooms);
 
-    socket.on('create room', function(data) {
+    socket.on('create room', (data) => {
         rooms.push(data.room_id);
         socket.join(data.room_id);
         io.emit('fetch rooms', rooms);
     });
 
-    socket.on('join', function(data) {
+    socket.on('join', (data) => {
         socket.join(data.room_id);
         io.emit('fetch rooms', rooms);
     });
 
-    socket.on('fetch message', function(room_id) {
+    socket.on('fetch message', (room_id) => {
         io.to(socket.id).emit('chat message init', messages[room_id]);
     });
 
-    socket.on('chat message', function(data) {
+    socket.on('chat message', (data) => {
         messages[data.room_id] = messages[data.room_id] || [];
         var log = {
             user_name: data.user_name,
